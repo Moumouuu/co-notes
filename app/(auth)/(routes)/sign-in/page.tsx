8ba69d606d7c08c6827dc54e13542830d56c7cea
Robin <95755219+Moumouuu@users.noreply.github.com>
2023-10-31 13:50:09 +0100

    Update page.tsx

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -3,8 +3,8 @@ import { Metadata } from "next";
 import UserAuthForm from "@/components/form/form-auth-user";
 
 export const metadata: Metadata = {
-  title: "EduFiche | Authentication",
-  description: "Page d'autentification de l'application EduFiche",
+  title: "Co-Notes | Authentication",
+  description: "Page d'autentification de l'application Co-Notes",
 };
 
 export default function AuthenticationPage() {
